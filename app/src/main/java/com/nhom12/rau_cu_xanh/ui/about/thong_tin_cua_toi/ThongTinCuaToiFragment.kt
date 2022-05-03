ca00ffc5603735dff655656aad7b5a42423be6f2
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-05-03 18:48:47 +0700

    implement date picker

--- app/src/main/java/com/nhom12/rau_cu_xanh/ui/about/thong_tin_cua_toi/ThongTinCuaToiFragment.kt
+++ app/src/main/java/com/nhom12/rau_cu_xanh/ui/about/thong_tin_cua_toi/ThongTinCuaToiFragment.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.adapters.AdapterViewBindingAdapter.setSelection
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.Navigation
-import com.nhom12.rau_cu_xanh.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.nhom12.rau_cu_xanh.R
+import kotlinx.android.synthetic.main.fragment_thong_tin_cua_toi.*
 import kotlinx.android.synthetic.main.return_to_title.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class ThongTinCuaToiFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,5 +37,30 @@ class ThongTinCuaToiFragment : Fragment() {
         back_to_about.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_thongTinCuaToiFragment_to_navigation_about))
         //Title text
         title_about_to?.setText(R.string.title_thong_tin_ca_nhan)
+
+        // Date picker implementation
+        date_picker_button.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            val fragmentManager = (activity as FragmentActivity).supportFragmentManager
+            datePicker.show(fragmentManager, "DatePicker")
+
+            // Setting up the event for when ok is clicked
+            datePicker.addOnPositiveButtonClickListener {
+                // formatting date in dd-mm-yyyy format.
+                val dateFormatter = SimpleDateFormat("dd-MM-yyyy")
+                val date = dateFormatter.format(Date(it))
+                Toast.makeText(activity?.applicationContext, "$date is selected", Toast.LENGTH_LONG)
+                    .show()
+            }
+            // Setting up the event for when cancelled is clicked
+            datePicker.addOnNegativeButtonClickListener {
+                Toast.makeText(activity?.applicationContext, "${datePicker.headerText} is cancelled", Toast.LENGTH_LONG).show()
+            }
+
+            // Setting up the event for when back button is pressed
+            datePicker.addOnCancelListener {
+                Toast.makeText(activity?.applicationContext, "Date Picker Cancelled", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
